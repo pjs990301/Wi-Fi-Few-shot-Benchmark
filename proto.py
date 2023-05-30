6f5f263f85c3527d571bf38f59f0bbf92ba495bf
Pio <p990301@gmail.com>
2023-05-30 12:08:13 +0900

    commit

--- proto.py
+++ proto.py
@@ -171,7 +171,7 @@ def load_protonet_vit():
         in_channels=1,  # 입력 채널 수
         patch_size=[22, 22],  # 패치 크기 (가로, 세로)
         embed_dim=64,  # 임베딩 차원
-        num_layers=16,  # Transformer 블록 수
+        num_layers=12,  # Transformer 블록 수
         num_heads=8,  # 멀티헤드 어텐션에서의 헤드 수
         mlp_dim=4,  # MLP의 확장 비율
         num_classes=4,  # 분류할 클래스 수
